48ba3bb78c6d680199f29c4f4d36d200bf31407f
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-07-10 15:45:59 +0530

    Garbage value causes and it's effects in c

---
+++ CS50/garbage.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int score[3];                   // Uninitialize value of score array
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", score[i]);   // Random values
+    }
+}
