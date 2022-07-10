69345a567f19990ec9175abf8747afae51de5c07
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-07-10 15:30:28 +0530

    Learn to allocate memory using malloc()

---
+++ CS50/copy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+
+    char *t = malloc( strlen(s) + 1 );          // Allocating blank memory
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        t[i] = s[i];                            // Copying data of s to t
+    }
+    
+
+    t[0] = toupper(t[0]);                       // Making changes to t(copy)
+
+    printf("s: %s\n",s);                        // Original copy is untouched!
+    printf("t: %s\n",t);
+}
