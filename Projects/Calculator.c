52e9146f9d3b1e8c62f48aeaa391b1c3fda25fec
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-07-19 00:53:04 +0530

    First Project Basic Calcuator

--- Projects/Calculator.c
+++ Projects/Calculator.c
@@ -2,46 +2,54 @@
 
 int main()
 {
+    // Variable Declaration
     float num1, num2, Result;
-    char opt;
-
-    printf("Choose the operation you want to perform +, -, *, /, %% : ");
-    scanf("%c", &opt);
+    int opt;
 
 
+    // Prompting user to select a operator
+    do
+    {
+    printf("Choose the operation you want to perform \n 1 - Addition(+)\n 2 - Subtraction(-)\n 3 - Multiplicatoin(*)\n 4 - Division(/)\n 5 - Modulus(%%)\n");
+    scanf("%i", &opt);
+    } while (opt > 5);
+
+
+    // Prompting user for num input
     printf("Enter the two numebr: ");
     scanf("%f", &num1);
     scanf("%f", &num2);
 
 
-    if ( opt == '+')
+    // Conditional Operation to perform on the basis of user selected operator
+    if ( opt == 1)
     {
         printf("You Selected: Addition");
-        Result = num1 + num2;
+        Result = num1 + num2;                       // Adding Two numbers
         printf("Sum of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
-    else if ( opt == '-')
+    else if ( opt == 2)
     {
         printf("You Selected: Subtraction");
-        Result = num1 - num2;
+        Result = num1 - num2;                       // Subtraction Two numbers
         printf("Difference of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
-    else if ( opt == '*')
+    else if ( opt == 3)
     {
         printf("You Selected: Multiplicatoin");
-        Result = num1 * num2;
+        Result = num1 * num2;                       // Multiplying Two numbers
         printf("Multiplication of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
-    else if ( opt == '/')
+    else if ( opt == 4)
     {
         printf("You Selected: Division");
-        Result = num1 - num2;
+        Result = num1 / num2;                       // Dividing Two numbers
         printf("Multiplication of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
-    else if ( opt == '%')
+    else if ( opt == 5)
     {
         printf("You Selected: Modulus");
-        Result = num1 - num2;
+        Result = (int)num1 % (int)num2;             // Modulos of Two number
         printf("Multiplication of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
     else
