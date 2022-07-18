e4fdccd9670da326c5560f25344f3eb9c0f3c44c
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-07-19 00:30:56 +0530

    First Project Basic Calcuator

--- Projects/Calculator.c
+++ Projects/Calculator.c
@@ -2,41 +2,52 @@
 
 int main()
 {
-    int num1, num2, Result;
+    float num1, num2, Result;
     char opt;
 
-    printf("Choose the operation you want to perform +, -, *, / : ");
+    printf("Choose the operation you want to perform +, -, *, /, %% : ");
     scanf("%c", &opt);
 
 
     printf("Enter the two numebr: ");
-    scanf("%i", &num1);
-    scanf("%i", &num2);
+    scanf("%f", &num1);
+    scanf("%f", &num2);
 
 
     if ( opt == '+')
     {
+        printf("You Selected: Addition");
         Result = num1 + num2;
-        printf("Sum of %i and %i is %i\n", num1, num2, Result);
+        printf("Sum of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
     else if ( opt == '-')
     {
+        printf("You Selected: Subtraction");
         Result = num1 - num2;
-        printf("Difference of %i and %i is %i\n", num1, num2, Result);
+        printf("Difference of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
     else if ( opt == '*')
     {
-        Result = num1 - num2;
-        printf("Multiplication of %i and %i is %i\n", num1, num2, Result);
+        printf("You Selected: Multiplicatoin");
+        Result = num1 * num2;
+        printf("Multiplication of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
     else if ( opt == '/')
     {
+        printf("You Selected: Division");
         Result = num1 - num2;
-        printf("Multiplication of %i and %i is %i\n", num1, num2, Result);
+        printf("Multiplication of %.2f and %.2f = %.2f\n", num1, num2, Result);
+    }
+    else if ( opt == '%')
+    {
+        printf("You Selected: Modulus");
+        Result = num1 - num2;
+        printf("Multiplication of %.2f and %.2f = %.2f\n", num1, num2, Result);
     }
     else
     {
-        printf("Error: Invalid Operator!");
+        printf("Error: Invalid Operator!\n");
     }
 
+    return 0;
 }
