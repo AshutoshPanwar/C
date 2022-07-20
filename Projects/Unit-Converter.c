cfc94bbdaeb86f6c11492a0384707fb2bcbd54e7
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-07-21 00:31:29 +0530

    Second Project Unit Convertor (Temp, Time, Mass, Length)

--- Projects/Unit-Converter.c
+++ Projects/Unit-Converter.c
@@ -1,12 +1,17 @@
+// Unit Convertor for Temperature, Time, Mass, Lenght;
+
 #include <stdio.h>
 
-int main()
+
+// Driver Code
+int main(void)
 {
+    // Prompt user to select the unit
     int unit_no;
-    printf("Select one of the unit: \n1 - Temperature\n2 - Time\n3 - Mass\n");
+    printf("Select one of the unit: \n1 - Temperature\n2 - Time\n3 - Mass\n4 - Length\n");
     scanf("%i", &unit_no);
 
-    if ( unit_no == 1 )
+    if ( unit_no == 1 )                 // If user select Temperature
     {
         int opt;
         float temp, F, C;
@@ -16,23 +21,23 @@ int main()
         printf("Enter the Temperature: ");
         scanf("%f", &temp);
 
-        if ( opt == 1)
+        if ( opt == 1)                              // If user select option 1.1
         {
             F = temp*(9/5) + 32;
             printf("%.2f in Celsius is %.2f in Fahrenheit\n", temp, F);
         }
-        else if ( opt == 2 )
+        else if ( opt == 2 )                        // If user select option 1.2
         {
             C = (temp - 32) * 5 / 9;
             printf("%.2f in Fahrenheit is %.2f in Celsius\n", temp, C);
         }
-        else
+        else                                        // If user enter unknown character in option 1
         {
             printf("ERROR!: Unknown Character!\n");
             return 1;
         }
     }
-    else if ( unit_no == 2 )
+    else if ( unit_no == 2 )                    // If user select Time
     {
         int opt, Time;
         float min, sec, hr;
@@ -42,43 +47,43 @@ int main()
         printf("Enter the Time: ");
         scanf("%i", &Time);
 
-        if ( opt == 1 )
+        if ( opt == 1 )                             // If user select option 2.1
         {
             min = Time / 60;
             printf("%i Second = %.2f Minute\n", Time, min);
         }
-        else if ( opt == 2)
+        else if ( opt == 2)                         // If user select option 2.2
         {
             hr = Time / 60;
             printf("%i Minute = %.2f Hour\n", Time, hr);
         }
-        else if ( opt == 3)
+        else if ( opt == 3)                         // If user select option 2.3
         {
             hr = Time / 120;
             printf("%i Second = %.2f Hour\n", Time, hr);
         }
-        else if ( opt == 4)
+        else if ( opt == 4)                         // If user select option 2.4
         {
             sec = Time * 60;
             printf("%i Minute = %.2f Second\n", Time, sec);
         }
-        else if ( opt == 5)
+        else if ( opt == 5)                         // If user select option 2.5
         {
             min = Time * 60;
             printf("%i Hour = %.2f Minute\n", Time, min);
         }
-        else if ( opt == 6)
+        else if ( opt == 6)                         // If user select option 2.6
         {
             sec = Time * 120;
             printf("%i Hour = %.2f Second\n", Time, sec);
         }
-        else
+        else                                        // If user enter unknown character in option 2
         {
             printf("ERROR!: Unknown Character!\n");
             return 1;
         }
     }
-    else if ( unit_no == 3 )
+    else if ( unit_no == 3 )                    // If user select Mass
     {
         int opt, val;
         float gr, kg, mil;
@@ -88,40 +93,91 @@ int main()
         printf("Enter the Time: ");
         scanf("%i", &val);
 
-        if ( opt == 1)
+        if ( opt == 1)                              // If user select option 3.1
         {
             gr = val * 1000;
             printf("%i Kilogram = %.2f Gram\n", val, gr);
         }
-        else if ( opt == 2)
+        else if ( opt == 2)                         // If user select option 3.2
         {
             kg = val / 1000000;
             printf("%i Milligram = %.2f Kilogram\n", val, kg);
         }
-        else if ( opt == 3)
+        else if ( opt == 3)                         // If user select option 3.3
         {
             gr = val / 1000;
             printf("%i Milligram = %.2f Gram\n", val, gr);
         }
-        else if ( opt == 4)
+        else if ( opt == 4)                         // If user select option 3.4
         {
             kg = val / 1000;
             printf("%i Gram = %.2f Kilogram\n", val, kg);
         }
-        else if ( opt == 5)
+        else if ( opt == 5)                         // If user select option 3.5
         {
             gr = val * 1000000;
             printf("%i Kilogram = %.2f Milligram\n", val, gr);
         }
-        else if ( opt == 6)
+        else if ( opt == 6)                         // If user select option 3.6
         {
             gr = val * 1000;
             printf("%i Gram = %.2f Milligram\n", val, gr);
         }
-        else
+        else                                        // If user enter unknown character in option 3
         {
             printf("ERROR!: Unknown Character!\n");
             return 1;
         }
+    }
+    else if (unit_no == 4)                  // If user select Length
+    {
+        int val, opt;
+        float km, m, cm;
+        printf("Select:\n1 - Kilometer to Meter\n2 - Meter to Centimeter\n3 - Kilometer to Centimeter\n4 - Meter to Kilometer\n5 - Centimeter to Meter\n6 - Centimeter to Kilometer\n");
+        scanf("%i", &opt);
+
+        printf("Enter the Time: ");
+        scanf("%i", &val);
+
+        if ( opt == 1)                                  // If user select option 4.1
+        {
+            m = val * 1000;
+            printf("%i Kilometer = %.2f Meter\n", val, m);
+        }
+        else if ( opt == 2)                             // If user select option 4.2
+        {
+            cm = val * 100;
+            printf("%i Meter = %.2f Centimeter\n", val, cm);
+        }
+        else if ( opt == 3)                             // If user select option 4.3
+        {
+            cm = val * 100000;
+            printf("%i Kilometer = %.2f Centimeter\n", val, cm);
+        }
+        else if ( opt == 4)                             // If user select option 4.4
+        {
+            km = val / 1000;
+            printf("%i Meter = %.2f Kilometer\n", val, km);
+        }
+        else if ( opt == 5)                             // If user select option 4.5
+        {
+            m = val / 100;
+            printf("%i Centimeter = %.2f Meter\n", val, m);
+        }
+        else if ( opt == 6)                             // If user select option 4.6
+        {
+            km = val / 100000;
+            printf("%i Centimeter = %.2f Kilometer\n", val, km);
+        }
+        else                                            // If user enter unknown character in option 4
+        {
+            printf("ERROR!: Unknown Character!\n");
+            return 1;
+        }
+    }
+    else                    // If user enter unknown character
+    {
+        printf("ERROR!: Unknown Character!\n");
+        return 1;
     }
 }
